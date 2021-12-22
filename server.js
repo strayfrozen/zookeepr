cae11f36389f8b1d6677d5be13e34576704ea983
Stray <strayfrozen@yahoo.com>
2021-12-22 10:31:19 -0600

    populated the and generated animal cards with post requests

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -97,6 +98,23 @@ function filterByQuery(query, animalsArray) {
       res.send(404);
     }
   });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
 
   app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
